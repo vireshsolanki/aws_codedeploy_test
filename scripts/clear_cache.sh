df2c6f59733e7ac45477d507106f051ff8e2ee34
vireshsolanki <vireshsolanki1027@gmail.com>
2024-04-04 13:05:50 +0530

    update cloudwatch

--- scripts/clear_cache.sh
+++ scripts/clear_cache.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Clear any cached data or temporary files
-echo "Clearing cache and temporary files..."
-sudo rm -rf /var/www/html/*
+# echo "Clearing cache and temporary files..."
+# sudo rm -rf /var/www/html/*
 sudo yum remove amazon-cloudwatch-agent.x86_64 -y
 sudo rm -rf /opt/*
